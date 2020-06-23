ed4e452a95c9573a6960b4d81963ca3079e70220
gsanders32 <gray.sanders@gmail.com>
2020-06-23 13:24:37 -0500

    added services code

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  url = 'https://api.punkapi.com/v2/';
+
+  async get(path){
+    return await this.httpClient.get(this.url + path).toPromise();
+  }
 }
--- src/app/services/beer.service.ts
+++ src/app/services/beer.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BeerService {
 
-  constructor() { }
+  constructor(private httpService: ApiService) { }
+
+  async getAll(){
+    const data = await this.httpService.get('beers');
+    return data;
 }
